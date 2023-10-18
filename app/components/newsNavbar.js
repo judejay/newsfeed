dfcdf9ddb5d8f9c9cca3c396b5fd4e08823db434
judejay <judejay26@gmail.com>
2023-10-18 09:51:10 +0100

    seach bar

--- app/components/newsNavbar.js
+++ app/components/newsNavbar.js
@@ -10,6 +10,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Image from 'next/image';
+import InputGroupText from 'react-bootstrap/esm/InputGroupText';
 function NewsNavbar() {
   return (
     <Navbar style={{backgroundColor: "#5d5df5"}} data-bs-theme="dark"  className="justify-content-between">
@@ -71,12 +72,22 @@ function NewsNavbar() {
       <Form inline="true">
         <Row>
           <Col xs="auto">
+            <InputGroup   >
             <Form.Control
-            style={{backgroundColor: "#4b4ac4"}} 
+           style={{backgroundColor: "#4242ae"}}
               type="text"
               placeholder="Enter your search here..."
-              className=" mr-sm-2"
+              className=" mr-xs-2"
             />
+            <InputGroupText
+            style={{backgroundColor: "#4242ae"}}
+            >
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+</svg>
+</InputGroupText>
+</InputGroup>
           </Col>
           <Col xs="auto">
           </Col>
